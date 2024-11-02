8a8477b0b5bdf65180c664c10ad02257fd4a661c
Brandon Fergerson <bfergerson@apache.org>
2024-11-02 15:56:44 -0300

    chore: build

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,17 +8,17 @@ val projectVersion: String by project
 group = "dev.voqal"
 version = project.properties["frameworkVersion"] as String? ?: projectVersion
 
-//configure<PublishingExtension> {
-//    repositories {
-//        maven {
-//            name = "GitHubPackages"
-//            url = uri("https://maven.pkg.github.com/voqal/assistant-framework")
+configure<PublishingExtension> {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/voqal/assistant-framework")
 //            credentials {
 //                username = System.getenv("GH_PUBLISH_USERNAME")?.toString()
 //                password = System.getenv("GH_PUBLISH_TOKEN")?.toString()
 //            }
-//        }
-//    }
+        }
+    }
 //
 //    publishing {
 //        publications {
@@ -31,7 +31,7 @@ version = project.properties["frameworkVersion"] as String? ?: projectVersion
 //            }
 //        }
 //    }
-//}
+}
 
 repositories {
     mavenCentral()

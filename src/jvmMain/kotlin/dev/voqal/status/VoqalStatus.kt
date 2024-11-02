5225f55a5705e2ff12811957baed6dfff2d9d129
Brandon Fergerson <bfergerson@apache.org>
2024-11-02 19:46:06 -0300

    wip: vaf impl

---
+++ src/jvmMain/kotlin/dev/voqal/status/VoqalStatus.kt
@@ -0,0 +1,12 @@
+package dev.voqal.status
+
+/**
+ * Represents the current status of the Voqal Assistant.
+ */
+enum class VoqalStatus {
+    ERROR,
+    DISABLED,
+    IDLE,
+    EDITING,
+    SEARCHING
+}
--- src/jvmMain/kotlin/dev/voqal/utils/SharedAudioCapture.kt
+++ src/jvmMain/kotlin/dev/voqal/utils/SharedAudioCapture.kt
@@ -1,10 +1,59 @@
 package dev.voqal.utils
 
 import com.intellij.openapi.project.Project
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 import java.util.ArrayList
 import java.util.concurrent.atomic.AtomicBoolean
+import javax.sound.sampled.AudioFormat
+import javax.sound.sampled.AudioInputStream
+import javax.sound.sampled.AudioSystem
 
+/**
+ * Allows multiple listeners to receive shared microphone audio data.
+ */
 class SharedAudioCapture(private val project: Project) {
+
+    companion object {
+        const val BUFFER_SIZE = 1532 //24khz -> 16khz = 512 samples
+        const val SAMPLE_RATE = 24000
+        val FORMAT = AudioFormat(SAMPLE_RATE.toFloat(), 16, 1, true, false)
+        val EMPTY_BUFFER = ByteArray(BUFFER_SIZE)
+
+        @JvmStatic
+        fun convertBytesToShorts(audioBytes: ByteArray): ShortArray {
+            val audioData = ShortArray(audioBytes.size / 2)
+            for (i in audioData.indices) {
+                audioData[i] = ((audioBytes[2 * i + 1].toInt() shl 8) or (audioBytes[2 * i].toInt() and 0xFF)).toShort()
+            }
+            return audioData
+        }
+
+        fun to16khz(byteArray: ByteArray): ByteArray {
+            val sourceFrameLength = byteArray.size / FORMAT.frameSize
+            val sourceStream = AudioInputStream(ByteArrayInputStream(byteArray), FORMAT, sourceFrameLength.toLong())
+
+            val targetFormat = AudioFormat(
+                16000f,
+                FORMAT.sampleSizeInBits,
+                FORMAT.channels,
+                FORMAT.encoding == AudioFormat.Encoding.PCM_SIGNED,
+                FORMAT.isBigEndian
+            )
+            val targetStream = AudioSystem.getAudioInputStream(targetFormat, sourceStream)
+            val outputStream = ByteArrayOutputStream()
+            val buffer = ByteArray(1024)
+            var bytesRead: Int
+            while (targetStream.read(buffer).also { bytesRead = it } != -1) {
+                outputStream.write(buffer, 0, bytesRead)
+            }
+            sourceStream.close()
+            targetStream.close()
+
+            return outputStream.toByteArray()
+        }
+    }
+
     data class AudioDetection(
         val voiceCaptured: AtomicBoolean = AtomicBoolean(false),
         val voiceDetected: AtomicBoolean = AtomicBoolean(false),

d7d3dbfd2c4f5a54b3a756df76d6227bb3f5106b
Brandon Fergerson <bfergerson@apache.org>
2024-11-02 15:53:22 -0300

    chore: build

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,30 +8,30 @@ val projectVersion: String by project
 group = "dev.voqal"
 version = project.properties["frameworkVersion"] as String? ?: projectVersion
 
-configure<PublishingExtension> {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/voqal/assistant-framework")
-            credentials {
-                username = System.getenv("GH_PUBLISH_USERNAME")?.toString()
-                password = System.getenv("GH_PUBLISH_TOKEN")?.toString()
-            }
-        }
-    }
-
-    publishing {
-        publications {
-            create<MavenPublication>("maven") {
-                groupId = project.group.toString()
-                artifactId = "assistant-framework"
-                version = project.version.toString()
-
-                from(components["kotlin"])
-            }
-        }
-    }
-}
+//configure<PublishingExtension> {
+//    repositories {
+//        maven {
+//            name = "GitHubPackages"
+//            url = uri("https://maven.pkg.github.com/voqal/assistant-framework")
+//            credentials {
+//                username = System.getenv("GH_PUBLISH_USERNAME")?.toString()
+//                password = System.getenv("GH_PUBLISH_TOKEN")?.toString()
+//            }
+//        }
+//    }
+//
+//    publishing {
+//        publications {
+//            create<MavenPublication>("maven") {
+//                groupId = project.group.toString()
+//                artifactId = "assistant-framework"
+//                version = project.version.toString()
+//
+//                from(components["kotlin"])
+//            }
+//        }
+//    }
+//}
 
 repositories {
     mavenCentral()

5cbe2e1847d0255b3eed19ca23b883e73e82b4c2
Brandon Fergerson <bfergerson@apache.org>
2024-11-09 20:18:13 -0300

    chore: clean

--- intellij-delegate/src/jvmMain/kotlin/dev/voqal/services/ProjectScopedService.kt
+++ intellij-delegate/src/jvmMain/kotlin/dev/voqal/services/ProjectScopedService.kt
@@ -1,6 +1,11 @@
 package dev.voqal.services
 
+import com.intellij.openapi.editor.RangeMarker
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiNamedElement
 import dev.voqal.utils.SharedAudioCapture
 import io.github.oshai.kotlinlogging.KLogger
 import kotlinx.coroutines.CoroutineScope
@@ -36,3 +41,58 @@ fun KLogger.errorChat(s: String, e: Throwable? = null) {
 fun Project.invokeLater(action: () -> Unit) {
     throw UnsupportedOperationException()
 }
+
+fun PsiElement.isFunction(): Boolean {
+    throw UnsupportedOperationException()
+}
+
+fun PsiElement.isCodeBlock(): Boolean {
+    throw UnsupportedOperationException()
+}
+
+fun PsiElement.isField(): Boolean {
+    throw UnsupportedOperationException()
+}
+
+fun PsiElement.isClass(): Boolean {
+    throw UnsupportedOperationException()
+}
+
+fun PsiElement.isFile(): Boolean {
+    throw UnsupportedOperationException()
+}
+
+fun PsiElement.isIdentifier(): Boolean {
+    throw UnsupportedOperationException()
+}
+
+fun PsiElement.isJvm(): Boolean {
+    throw UnsupportedOperationException()
+}
+
+fun PsiElement.isPython(): Boolean {
+    throw UnsupportedOperationException()
+}
+
+fun PsiElement.isGo(): Boolean {
+    throw UnsupportedOperationException()
+}
+
+fun PsiElement.getCodeBlock(): PsiElement? {
+    throw UnsupportedOperationException()
+}
+
+fun PsiFile.getFunctions(): List<PsiNamedElement> {
+    throw UnsupportedOperationException()
+}
+
+fun PsiFile.getFields(): List<PsiNamedElement> {
+    throw UnsupportedOperationException()
+}
+
+fun PsiFile.getClasses(): List<PsiNamedElement> {
+    throw UnsupportedOperationException()
+}
+
+val RangeMarker.range: TextRange?
+    get() = throw UnsupportedOperationException()

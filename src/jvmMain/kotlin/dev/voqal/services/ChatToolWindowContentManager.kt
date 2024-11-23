eba905ffcc8436a75685a2b777b09efbdb6fc98c
Brandon Fergerson <bfergerson@apache.org>
2024-11-22 22:57:54 -0300

    chat ui

--- src/jvmMain/kotlin/dev/voqal/services/ChatToolWindowContentManager.kt
+++ src/jvmMain/kotlin/dev/voqal/services/ChatToolWindowContentManager.kt
@@ -6,4 +6,10 @@ interface ChatToolWindowContentManager {
     fun addUserMessage(transcript: String, speechId: String? = null)
     fun addAssistantMessage(transcript: String, speechId: String? = null)
     fun addAssistantToolResponse(voqalTool: VoqalTool, callId: String, args: String, it: Any?)
+    fun getConversation(): List<ChatMessage>
+
+    data class ChatMessage(
+        val text: String,
+        val isUser: Boolean
+    )
 }

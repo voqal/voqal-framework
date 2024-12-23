6f784e36f0163468e63366ea47208c7dd4911087
Brandon Fergerson <bfergerson@apache.org>
2024-12-22 19:53:49 -0500

    clean

--- src/jvmTest/kotlin/dev/voqal/core/MockContextService.kt
+++ src/jvmTest/kotlin/dev/voqal/core/MockContextService.kt
@@ -1,9 +1,5 @@
 package dev.voqal.core
 
-import com.intellij.formatting.Block
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.util.TextRange
-import com.intellij.psi.PsiFile
 import dev.voqal.assistant.VoqalDirective
 import dev.voqal.services.VoqalContextService
 
@@ -11,18 +7,7 @@ class MockContextService : VoqalContextService {
 
     override fun getTokenCount(text: String) = -1
 
-    override fun getSelectedTextEditor(): Editor? {
-        TODO("Not yet implemented")
-    }
-
     override fun cropAsNecessary(fullDirective: VoqalDirective): VoqalDirective {
-        TODO("Not yet implemented")
-    }
-
-    override fun getOpeningBlockAt(
-        psiFile: PsiFile,
-        editRange: TextRange
-    ): Block? {
         TODO("Not yet implemented")
     }
 }
